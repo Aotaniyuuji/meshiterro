459bc6fd3c5ba851450a7f17627c3f64c5ee6f55
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-13 04:24:38 +0000

    [update]4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,7 +5,7 @@ class PostImage < ApplicationRecord
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/aseets/images/no_image.jpg')
-      image.attsch(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
     end
     image
   end
