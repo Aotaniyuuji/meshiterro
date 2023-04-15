c7a37a3fe4189f5e61507fbaeffc8970893502c1
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-15 11:34:09 +0000

    [update]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+
     @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(current_user.id)
@@ -18,5 +27,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name,:profile,:profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
